2c3bcbd8a15d01705b1cb56e67bcfa9d4bed4c35
AOMuiz <abdulwasiumuiz@gmail.com>
2024-07-24 19:40:20 +0100

    ...

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -16,11 +16,17 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
 export const almarai = Almarai({
   variable: "--font-almaria",
   subsets: ["arabic"],
   weight: ["300", "400", "700", "800"],
+  display: "swap",
 });
 
 export const Val = {
